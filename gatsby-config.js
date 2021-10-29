7fb3b22069dad01c69cc34925585c84891b50592
Théodore <theodoreyip19@gmail.com>
2021-10-28 21:25:59 -0700

    display projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `projects`,
+        path: `${__dirname}/projects`,
+      }
+    },
+    `gatsby-plugin-postcss`,
+  ],
 };
